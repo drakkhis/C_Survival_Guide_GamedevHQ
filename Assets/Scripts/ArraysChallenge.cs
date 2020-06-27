0f645de1f2a92a57f36358532e64be8fc2fffd91
wiggansj <wiggansj@live.com>
2020-06-27 23:56:29 +0200

    Arrays Challlenge 1

---
+++ Assets/Scripts/ArraysChallenge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysChallenge : MonoBehaviour
+{
+    [SerializeField]
+    private string[] _names;
+    [SerializeField]
+    private int[] _ages;
+    [SerializeField]
+    private string[] _cars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Name : " + _names[4] + " Age: " + _ages[4] + " Car: " + _cars[4]);
+        }
+    }
+}
