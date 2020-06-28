23bab3647e91d6ed37d5d2800b0625613a6d33d8
wiggansj <wiggansj@live.com>
2020-06-28 02:01:51 +0200

    Functions vs methods Final Challange

--- Assets/Scripts/FunctionsMethodsChallenge.cs
+++ Assets/Scripts/FunctionsMethodsChallenge.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class FunctionsMethodsChallenge : MonoBehaviour
 {
     [SerializeField]
-    private int _healthMax;
+    private Vector3[] _positions;
     [SerializeField]
-    private int _health;
+    private GameObject _player;
+
     // Start is called before the first frame update
     void Start()
     {
-        _health = _healthMax;
+        
     }
 
     // Update is called once per frame
@@ -19,23 +20,18 @@ public class FunctionsMethodsChallenge : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (_health > 0)
-                Damage(Random.Range(5, 11));
-            else
-                Debug.Log("Player Is Dead");
-            Debug.Log("Player has " + _health + " health remaining");
+            SetPosition(_player, _positions[RandomIndex(_positions)]);
         }
+
     }
 
-    private void Damage(int amount)
+    private void SetPosition(GameObject obj, Vector3 pos)
     {
-        _health -= amount;
-        Debug.Log("Player has taken " + amount + " damage");
-        if (_health < 1)
-        {
-            _health = 0;
-            Debug.Log("Player Is Dead");
-        }
-            
+        obj.transform.position = pos;
+    }
+
+    private int RandomIndex(Vector3[] posArray)
+    {
+        return Random.Range(0, posArray.Length);
     }
 }
