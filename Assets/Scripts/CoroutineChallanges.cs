9fb977ef9b7231da27ae38d15354898dbc2c1518
wiggansj <wiggansj@live.com>
2020-06-28 02:23:38 +0200

    Coroutine challenge 1

---
+++ Assets/Scripts/CoroutineChallanges.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineChallanges : MonoBehaviour
+{
+    private bool isVisable = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && isVisable)
+        {
+            StartCoroutine(Vanish());
+        }
+    }
+
+    IEnumerator Vanish()
+    {
+        isVisable = false;
+        transform.GetComponent<MeshRenderer>().enabled = false;
+        yield return new WaitForSeconds(5.0f);
+        transform.GetComponent<MeshRenderer>().enabled = true;
+        isVisable = true;
+    }
+}
