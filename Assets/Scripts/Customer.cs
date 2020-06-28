abe68e8a942307f80c93e84e6ea53d071c076075
wiggansj <wiggansj@live.com>
2020-06-28 14:32:08 +0200

    Classes Challenge 1

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,24 @@
+﻿
+[System.Serializable]
+public class Customer
+{
+    public string FirstName;
+    public string LastName;
+    public uint Age;
+    public string Gender;
+    public string Occupation;
+
+    public Customer()
+    {
+
+    }
+
+    public Customer(string firstName, string lastName, uint age, string gender, string occupation)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Gender = gender;
+        this.Occupation = occupation;
+    }
+}
