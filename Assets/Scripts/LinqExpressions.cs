6931b74df76ec19a911ba1eb0f599ae1933014bd
wiggansj <wiggansj@live.com>
2020-06-30 00:11:31 +0200

    LINQ Challenge 2

--- Assets/Scripts/LinqExpressions.cs
+++ Assets/Scripts/LinqExpressions.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.Experimental.Rendering;
 
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int ID;
+    public int buff;
+
+
+}
+
 public class LinqExpressions : MonoBehaviour
 {
-    public int[] grades = {12,54,85,64,20,100,45,87,96,35,42,01,25,85,4,12,69,48,25,12,84,75,96,32,12,05,85,65,15,74 };
+    public Item[] Items;
 
     // Start is called before the first frame update
     void Start()
     {
-        var c = grades.Where(g => g > 69);
-        foreach(int n in c)
+        var isInInventory = Items.Any(i => i.ID == 3);
+        Debug.Log(isInInventory);
+        var highBuffs = Items.Where(i => i.buff > 20);
+        foreach(Item i in highBuffs)
         {
-            Debug.Log(n);
+            Debug.Log(i.name);
         }
+        var buffAverage = Items.Average(n => n.buff);
+        Debug.Log("Average of all Buffs is: " + buffAverage);
     }
 
 }
