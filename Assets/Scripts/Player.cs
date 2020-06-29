3855c4dd8f5e4c5d3e90e8d2f24d68607bb921f1
wiggansj <wiggansj@live.com>
2020-06-29 03:41:12 +0200

    Properties Chalange 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public string Name { get; set; }
+
+    public float Speed { get; } = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        ChangeColor.ColorChange(this.gameObject, Color.red);
+        Debug.Log(Speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeColor.ColorChange(this.gameObject);
-        }
+
         
     }
 }
