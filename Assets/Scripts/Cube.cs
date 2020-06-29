cfd8e4a200a8a7e953f87f90c002100eedc5ec74
wiggansj <wiggansj@live.com>
2020-06-29 22:20:58 +0200

    Delegate/Event Challenge 1

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main.onSpaceBar += ChangePos;
+    }
+
+    public void ChangePos()
+    {
+        this.transform.position = new Vector3(5, 2, 0);
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    public delegate void OnSpaceBar();
+    public static event OnSpaceBar onSpaceBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onSpaceBar != null)
+                onSpaceBar();
+        }
+    }
+}
