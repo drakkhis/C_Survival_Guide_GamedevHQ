03f51c1c3e510c032c1349dcf635e9b8874d6b92
wiggansj <wiggansj@live.com>
2020-06-29 02:33:59 +0200

    Static Types Challenge 1

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ChangeColor
+{
+    public static void ColorChange(GameObject obj, Color color)
+    {
+        obj.GetComponent<Renderer>().material.color = color;
+    }
+    /// <summary>
+    /// Random Color
+    /// </summary>
+     public static void ColorChange(GameObject obj)
+    {
+        
+        var color = new Color(Random.value, Random.value, Random.value);
+        obj.GetComponent<Renderer>().material.color = color;
+    }
+}
