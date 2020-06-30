7f0c65d6b0e99e78b1651c67a3a5c43a42dee4a6
wiggansj <wiggansj@live.com>
2020-06-30 03:27:44 +0200

    Pool Manager Challenge

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Invoke("Hide", 1);
+    }
+    void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoSingleton<SpawnManager>
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+}
