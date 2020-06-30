512570ec040be5607eaab82cb9dad10fea5b1c6b
wiggansj <wiggansj@live.com>
2020-06-30 13:51:59 +0200

    command manager 0.1

---
+++ Assets/Scripts/Managers/CommandManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CommandManager : MonoSingleton<CommandManager>
+{
+    private List<ICommand> _commandBuffer = new List<ICommand>();
+    public override void Init()
+    {
+        base.Init();
+
+    }
+
+    public void Record(ICommand command)
+    {
+        _commandBuffer.Add(command);
+    }
+
+    public IEnumerator Play()
+    {
+        foreach (var command in _commandBuffer)
+        {
+            command.Exicute();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public IEnumerator Rewind()
+    {
+        _commandBuffer.Reverse();
+        foreach (var command in _commandBuffer)
+        {
+
+            command.Exicute();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void Done()
+    {
+        GameObject.FindGameObjectsWithTag("Cube").ToList().ForEach(g => g.GetComponent<MeshRenderer>().material.color = Color.white);
+    }
+
+    public void Reset()
+    {
+        _commandBuffer.Clear();
+    }
+
+}
---
+++ Assets/Scripts/UserClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserClick : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrgin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrgin, out hitInfo))
+            {
+                if (hitInfo.collider.CompareTag("Cube"))
+                {
+                    ICommand click = new ClickCommand(hitInfo.collider.gameObject, new Color(Random.value, Random.value, Random.value));
+                    click.Exicute();
+                    CommandManager.Instance.Record(click);
+                }
+            }
+        }
+    }
+}
